475571bc4c1e2d6dcd7083ae06ba27aa3f832815
Fahd El Haraka <elharakafahd@icloud.com>
2023-10-15 14:03:45 +0000

    Update api.py

--- api.py
+++ api.py
@@ -13,69 +13,32 @@ Unauthorized use, duplication, modification, or distribution is strictly prohibi
 Contact fahd@web3dev.ma for permissions and inquiries.
 """
 
-from imports import requests, json
+from imports import requests ,json
 from config import BSCSCAN_API_KEY
 
 def get_token_abi(tokentobuy):
-    """
-    Fetches the ABI of a token from BscScan.
-    
-    Parameters:
-    tokentobuy (str): The contract address of the token.
-
-    Returns:
-    dict: The ABI of the token.
-    """
-    url = (
-        f"https://api.bscscan.com/api?"
-        f"module=contract&action=getabi&address={tokentobuy}"
-    )
+    url = f"https://api.bscscan.com/api?module=contract&action=getabi&address={tokentobuy}"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
     abi = json.loads(data['result'])
     return abi
 
+# Get Token balance (sell simulation)
 def get_token_balance(tokentobuy, my_address):
-    """
-    Fetches the balance of a specified token for a given address from BscScan.
-    
-    Parameters:
-    tokentobuy (str): The contract address of the token.
-    my_address (str): The address to check the balance for.
-
-    Returns:
-    int: The balance of the token.
-    """
-    url = (
-        f"https://api.bscscan.com/api?"
-        f"module=account&action=tokenbalance&contractaddress={tokentobuy}"
-        f"&address={my_address}&tag=latest&apikey={BSCSCAN_API_KEY}"
-    )
+    url = f"https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress={tokentobuy}&address={my_address}&tag=latest&apikey={BSCSCAN_API_KEY}"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
-    balance = int(data['result'])
+    balance = int(data['result'])  # Convert string to integer
     return balance
 
-def get_wbnb_balance(wbnb_address, my_address):
-    """
-    Fetches the balance of WBNB for a given address from BscScan.
-    
-    Parameters:
-    wbnb_address (str): The contract address of WBNB.
-    my_address (str): The address to check the balance for.
 
-    Returns:
-    int: The balance of WBNB.
-    """
-    url = (
-        f"https://api.bscscan.com/api?"
-        f"module=account&action=tokenbalance&contractaddress={wbnb_address}"
-        f"&address={my_address}&tag=latest&apikey={BSCSCAN_API_KEY}"
-    )
+# Get WBNB balance
+def get_wbnb_balance(wbnb_address, my_address):
+    url = f"https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress={wbnb_address}&address={my_address}&tag=latest&apikey={BSCSCAN_API_KEY}"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
-    balance = int(data['result'])
+    balance = int(data['result'])  # Convert string to integer
     return balance
