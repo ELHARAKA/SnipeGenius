d84e19dc9b9df19fd28e540b45b6f8fd70c1c633
Fahd El Haraka <elharakafahd@icloud.com>
2023-10-15 00:10:00 +0000

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,24 @@
 
 """
 © Copyright 2023
-Proprietary Software by Fahd El Haraka, 2023. Unauthorized use, duplication, modification, or distribution is strictly prohibited. Contact fahd@web3dev.ma for permissions and inquiries.
+Proprietary Software by Fahd El Haraka, 2023.
+Unauthorized use, duplication, modification, or distribution is strictly prohibited.
+Contact fahd@web3dev.ma for permissions and inquiries.
 """
 
-from imports import *
+import sys
+import json
+import logging
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
 
 # Configure logging for trade history to a file.
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 trade_logger = logging.getLogger('trade_logger')
 trade_handler = logging.FileHandler('trade_history.log')
 trade_handler.setLevel(logging.INFO)
@@ -27,22 +38,18 @@ if w3.is_connected():
     w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 else:
     logging.error("Node connection failed.")
-    exit(1)
+    sys.exit(1)
 
-# Your wallet details 
 my_address = '0xYour_Wallet_Address'
 private_key = '0xYour_Private_Key'
-
-# BSCSCAN API
-BSCSCAN_API_KEY = "YOUR_API_KEY" # Obtain it from https://bscscan.com/myapikey
-
-# Addresses for WBNB Token, Router, and Factory in PancakeSwap.
 wbnb_address = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
 router_address = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
 factory_address = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
-
-#Event Topic for "CreatedPair"
 pair_created_topic = '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9'
+minimum_sleep = 3
+
+# BSCSCAN API
+BSCSCAN_API_KEY = "YOUR_API_KEY" # Obtain it from https://bscscan.com/myapikey
 
 # Load ABI (Application Binary Interface) from a JSON file
 def load_abi_from_file(filename):
@@ -64,6 +71,3 @@ event_filter = w3.eth.filter({
     'address': factory_address,
     'topics': [pair_created_topic]
 })
-
-# Minimum sleep duration
-minimum_sleep = 3
