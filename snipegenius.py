2f6690ccf4b9869c0f644626cb51277630cc6c5c
Fahd El Haraka <elharakafahd@icloud.com>
2023-10-15 13:36:30 +0000

    Update snipegenius.py

--- snipegenius.py
+++ snipegenius.py
@@ -13,20 +13,24 @@ Unauthorized use, duplication, modification, or distribution is strictly prohibi
 Contact fahd@web3dev.ma for permissions and inquiries.
 """
 
-from imports import *
+from imports import logging, requests, json, time
 from api import get_token_abi, get_token_balance
 
+# pylint: disable=too-many-locals
+
 def is_blacklisted(tokentobuy, w3):
+    """Check if the token owner is blacklisted."""
     token_abi = get_token_abi(tokentobuy)
     token_contract = w3.eth.contract(address=tokentobuy, abi=token_abi)
     owner_address = token_contract.functions.owner().call()
 
-    with open('blacklist.txt', 'r') as file:
+    with open('blacklist.txt', 'r', encoding='utf-8') as file:
         blacklist = file.read().splitlines()
 
     return owner_address.lower() in blacklist
 
 def perform_safety_check(tokentobuy, pair_address, chain_id):
+    """Performing safety checks"""
     honeypot_url = (
         f"https://api.honeypot.is/v2/IsHoneypot?address={tokentobuy}"
         f"&pair={pair_address}&chainID={chain_id}"
@@ -101,40 +105,38 @@ def perform_safety_check(tokentobuy, pair_address, chain_id):
     return passed_checks == total_checks  # Return True if safety rate is 90% or higher, False otherwise
 
 # Simulate Transactions to inspect any issues during buy/sell
-def simulate_transactions(my_address, tokentobuy, router, wbnb, private_key, w3):
+def simulate_transactions(MY_ADDRESS, tokentobuy, router, wbnb, PRIVATE_KEY, w3):
+    """Simulate BUY/SELL Transactions before executing actual buy transactions."""
     amount_to_buy = w3.to_wei('0.00001', 'ether')  # 0.00001 WBNB in Wei
 
     # Simulate Buy
     buy_txn = router.functions.swapExactETHForTokens(
         0,  # set to 0 to accept any amount of tokens
         [wbnb.address, tokentobuy],
-        my_address,
+        MY_ADDRESS,
         int(time.time()) + 120
     ).build_transaction({
-        'from': my_address,
+        'from': MY_ADDRESS,
         'value': amount_to_buy,
         'gas': 250000,
         'gasPrice': w3.eth.gas_price,
-        'nonce': w3.eth.get_transaction_count(my_address),
+        'nonce': w3.eth.get_transaction_count(MY_ADDRESS),
         'chainId': 56
     })
-
-    signed_buy_txn = w3.eth.account.sign_transaction(buy_txn, private_key)
+    signed_buy_txn = w3.eth.account.sign_transaction(buy_txn, PRIVATE_KEY)
     buy_txn_hash = w3.eth.send_raw_transaction(signed_buy_txn.rawTransaction)
     buy_txn_receipt = w3.eth.wait_for_transaction_receipt(buy_txn_hash)
-
     if buy_txn_receipt.status != 1:
         logging.error('Buy simulation failed.')
         return False  # Simulation failed at buy step
-    else:
-        logging.info(f"Buy Simulation Passed: TX HASH {buy_txn_hash.hex()}")
-    
+    logging.info(f"Buy Simulation Passed: TX HASH {buy_txn_hash.hex()}")
+
     time.sleep(15)  # Wait for TX to get confirmed
-    
-    token_balance = get_token_balance(tokentobuy, my_address) # Get Token Balance
+
+    token_balance = get_token_balance(tokentobuy, MY_ADDRESS) # Get Token Balance
     if token_balance == 0:
         logging.error('Balance is Zero')
-        return False 
+        return False
     else:
         logging.info(f'Token balance = {token_balance}')
 
@@ -142,13 +144,13 @@ def simulate_transactions(my_address, tokentobuy, router, wbnb, private_key, w3)
     token_abi = get_token_abi(tokentobuy)
     token_contract = w3.eth.contract(address=tokentobuy, abi=token_abi)
     approve_txn = token_contract.functions.approve(router.address, token_balance).build_transaction({
-        'from': my_address,
+        'from': MY_ADDRESS,
         'gas': 100000,
         'gasPrice': w3.eth.gas_price,
-        'nonce': w3.eth.get_transaction_count(my_address),
+        'nonce': w3.eth.get_transaction_count(MY_ADDRESS),
         'chainId': 56
     })
-    signed_approve_txn = w3.eth.account.sign_transaction(approve_txn, private_key)
+    signed_approve_txn = w3.eth.account.sign_transaction(approve_txn, PRIVATE_KEY)
     approve_txn_hash = w3.eth.send_raw_transaction(signed_approve_txn.rawTransaction)
     w3.eth.wait_for_transaction_receipt(approve_txn_hash)
     approve_txn_receipt = w3.eth.wait_for_transaction_receipt(approve_txn_hash)
@@ -156,7 +158,7 @@ def simulate_transactions(my_address, tokentobuy, router, wbnb, private_key, w3)
         logging.info('Approval Simulation successful.')
     else:
         logging.error('Approval Simulation failed.')
-    
+
     time.sleep(15) # Wait for TX to get confirmed
 
     # Simulate Sell
@@ -164,17 +166,17 @@ def simulate_transactions(my_address, tokentobuy, router, wbnb, private_key, w3)
         token_balance,
         0,  # set to 0 to accept any amount of ETH
         [tokentobuy, wbnb.address],
-        my_address,
+        MY_ADDRESS,
         int(time.time()) + 120
     ).build_transaction({
-        'from': my_address,
+        'from': MY_ADDRESS,
         'gas': 250000,
         'gasPrice': w3.eth.gas_price,
-        'nonce': w3.eth.get_transaction_count(my_address),
+        'nonce': w3.eth.get_transaction_count(MY_ADDRESS),
         'chainId': 56
     })
 
-    signed_sell_txn = w3.eth.account.sign_transaction(sell_txn, private_key)
+    signed_sell_txn = w3.eth.account.sign_transaction(sell_txn, PRIVATE_KEY)
     sell_txn_hash = w3.eth.send_raw_transaction(signed_sell_txn.rawTransaction)
     sell_txn_receipt = w3.eth.wait_for_transaction_receipt(sell_txn_hash)
 
@@ -182,16 +184,19 @@ def simulate_transactions(my_address, tokentobuy, router, wbnb, private_key, w3)
 
     if sell_txn_receipt.status != 1:
         logging.error('Sell Simulation Failed, Run the fuck away.')
-        return False 
+        return False
     else:
         logging.info(f"Sell Simulation Passed: TX HASH {sell_txn_hash.hex()}")
 
-def check_token_safety(tokentobuy, pair_address, chain_id, my_address, private_key, w3, router, wbnb):
+    return True
+
+def check_token_safety(tokentobuy, pair_address, chain_id, MY_ADDRESS, PRIVATE_KEY, w3, router, wbnb):
+    """Determine if token is safe or not"""
     time.sleep(30)  # Wait for 30 seconds before the first check
     first_check = perform_safety_check(tokentobuy, pair_address, chain_id)
 
     if first_check:
-        simulation_result = simulate_transactions(my_address, tokentobuy, router, wbnb, private_key, w3)
+        simulation_result = simulate_transactions(MY_ADDRESS, tokentobuy, router, wbnb, PRIVATE_KEY, w3)
         return simulation_result
 
     return False
