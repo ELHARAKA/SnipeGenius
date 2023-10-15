358f68779df9740d1b40dfd95557f5186f270349
Fahd El Haraka <elharakafahd@icloud.com>
2023-10-15 14:05:21 +0000

    Update snipe.py

--- snipe.py
+++ snipe.py
@@ -13,29 +13,29 @@ Unauthorized use, duplication, modification, or distribution is strictly prohibi
 Contact fahd@web3dev.ma for permissions and inquiries.
 """
 
-from config import trade_logger, event_filter, MINIMUM_SLEEP
+from config import trade_logger, event_filter, minimum_sleep
 from imports import logging, time
 from transactions import handle_event
 
 # Set up logging
 trade_logger.info("Sniping Started, Fetching for new events...")
 
-IS_PROCESSING = False
+is_processing = False
 
 # Main loop (Events Check & Execute Trades)
 while True:
-    if not IS_PROCESSING:
+    if not is_processing:
         try:
             events = event_filter.get_new_entries()
 
             if events:
-                IS_PROCESSING = True  # set flag to True when event is found
+                is_processing = True  # set flag to True when event is found
                 logging.info(f"Found {len(events)} new event(s).")
                 for event in events:
                     handle_event(event)  # Handle each new event
-                IS_PROCESSING = False  # reset flag to False after handling event
+                is_processing = False  # reset flag to False after handling event
 
-            sleep_duration = max(MINIMUM_SLEEP, len(events))
+            sleep_duration = max(minimum_sleep, len(events))
             time.sleep(sleep_duration)
 
         except Exception as e:
