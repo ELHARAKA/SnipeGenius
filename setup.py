7799d73d8885840ab0be76b4a5933fa443ee4887
FahdWeb3Dev <35965906+ELHARAKA@users.noreply.github.com>
2023-10-25 14:42:10 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
 
-# Read the content of requirements.txt
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='SnipeGenius',
     version='1.5.0',
@@ -13,7 +9,16 @@ setup(
     author_email='fahd@web3dev.ma',
     license='https://github.com/ELHARAKA/SnipeGenius/blob/main/LICENSE',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        'pyfiglet',
+        'colorlog',
+        'termcolor',
+        'web3',
+        'requests',
+        'cryptography',
+        'google-api-python-client==2.104.0',
+        'pwinput'
+    ],
     python_requires='>=3.6',
     entry_points={
         'console_scripts': [
