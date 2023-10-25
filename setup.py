38ffdfa3898c14670471447762fac3e1520bc5a7
Fahd El Haraka <elharakafahd@icloud.com>
2023-10-25 13:20:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,15 @@ with open('requirements.txt') as f:
 
 setup(
     name='SnipeGenius',
-    version='1.0.2',
+    version='1.5.0',
+    description='A sniping bot for detecting and trading new token pairs safely',
+    url='https://github.com/ELHARAKA/SnipeGenius',
+    author='Fahd El Haraka',
+    author_email='fahd@web3dev.ma',
+    license='https://github.com/ELHARAKA/SnipeGenius/blob/main/LICENSE',
     packages=find_packages(),
     install_requires=requirements,
+    python_requires='>=3.6',
     entry_points={
         'console_scripts': [
             'snipegenius = snipe:main',
